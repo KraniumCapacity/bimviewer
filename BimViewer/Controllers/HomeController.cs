c7ed8495f1b8d14aa6b683d3110ecaff232e8aec
Anders <lordpeppezaltaz22@hotmail.com>
2016-10-17 09:10:51 +0200

    Patch 1

---
+++ BimViewer/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BimViewer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ModelViewer()
+        {
+            ViewBag.Message = "This is the model viewer";
+
+            return View();
+        }
+    }
+}
